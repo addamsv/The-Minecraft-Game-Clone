49f880ff95ffb6b255c46abf456e5a83fa445bcb
Rastsislau Kharlanau <tatsmaki@yahoo.com>
2020-12-29 04:41:14 +0300

    feat: simple collision

--- minecraft/src/conltollers/modules/animationFrame.ts
+++ minecraft/src/conltollers/modules/animationFrame.ts
@@ -1,3 +1,4 @@
+import { Vector3 } from 'three';
 import GameInterface from '../../interfaces/gameInterface';
 
 const WEIGHT = 50.0;
@@ -12,19 +13,22 @@ class AnimationFrame {
 
   animate() {
     const { game } = this;
+    game.jump = false;
     requestAnimationFrame(this.animate.bind(this));
     const time = performance.now();
     if (game.control.isLocked === true) {
-      game.raycaster.ray.origin.copy(game.control.getObject().position);
-      game.raycaster.ray.origin.y -= 10;
+      game.raycaster.ray.origin.copy(game.camera.position);
+      game.raycaster.ray.origin.y -= 15;
       const intersections = game.raycaster.intersectObjects(game.collision);
       const onObject = intersections.length > 0;
+
       const delta = (time - game.time) / 1000;
       game.speed.x -= game.speed.x * 10.0 * delta;
       game.speed.z -= game.speed.z * 10.0 * delta;
       game.speed.y -= 9.8 * WEIGHT * delta;
       game.direction.z = Number(game.forward) - Number(game.backward);
       game.direction.x = Number(game.right) - Number(game.left);
+
       game.direction.normalize();
       if (game.forward || game.backward) {
         game.speed.z -= game.direction.z * 400.0 * delta;
@@ -32,18 +36,27 @@ class AnimationFrame {
       if (game.left || game.right) {
         game.speed.x -= game.direction.x * 400.0 * delta;
       }
-      if (onObject === true) {
+
+      if (onObject) {
         game.speed.y = Math.max(0, game.speed.y);
         game.jump = true;
       }
+
+      const ray = new Vector3();
+      const { x, y, z } = game.camera.position;
+      ray.x = x; ray.y = y - 10; ray.z = z;
+      game.raycaster.ray.origin.copy(ray);
+      const wallCheck = game.raycaster.intersectObjects(game.collision);
+
+      if (wallCheck.length) {
+        game.speed.z = 0;
+        game.speed.x = 0;
+        game.speed.y += 30;
+      }
+
       game.control.moveRight(-game.speed.x * delta);
       game.control.moveForward(-game.speed.z * delta);
-      game.control.getObject().position.y += (game.speed.y * delta);
-      if (game.control.getObject().position.y < 10) {
-        game.speed.y = 0;
-        game.control.getObject().position.y = 10;
-        game.jump = true;
-      }
+      game.camera.position.y += (game.speed.y * delta);
     }
     game.time = time;
     game.renderer.render(game.scene, game.camera);
