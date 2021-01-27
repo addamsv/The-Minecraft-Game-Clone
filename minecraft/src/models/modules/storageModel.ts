4d75cc621b872e0840fefe3535c5c8626031be0a
addamsv <swares@mail.ru>
2021-01-27 13:38:12 +0300

    refactor: got rid of comments

--- minecraft/src/models/modules/storageModel.ts
+++ minecraft/src/models/modules/storageModel.ts
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable class-methods-use-this */
 import StorageModelInterface from './storageModelInterface';
 import ServerSocketModelInterface from './ServerSocketModelInterface';
 import MainControllerInterface from '../../controllers/mainControllerInterface';
