799f250f456d9479e02281a44201086598712c74
Serge <swares@mail.ru>
2021-02-01 15:16:05 +0300

    fix: fixed env variables

--- rs-clone-server/app-config.ts
+++ rs-clone-server/app-config.ts
@@ -5,14 +5,7 @@ export default {
   collectionTokens: 'tokens',
   collectionPlayerSettings: 'player_settings',
   collectionPlayerStatistics: 'player_statistics',
-  MONGO_DB_URL: process.env.MONGO_DB_URL || 'mongodb+srv://rsmaincraftserver:rsmaincraftserver2020q3@cluster0.fnt9p.mongodb.net/?retryWrites=true&w=majority',
-  TOKEN_KEY: process.env.TOKEN_KEY || '1a2b-3c4d-5e6f-7g8h',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgres://postgres:@localhost:5432/postgres',
+  MONGO_DB_URL: process.env.MONGO_DB_URL,
+  TOKEN_KEY: process.env.TOKEN_KEY,
+  DATABASE_URL: process.env.DATABASE_URL,
 };
-// export default {
-//   dbName: 'rs-clone-server',
-//   collectionName: 'players',
-//   MONGO_DB_URL: process.env.MONGO_DB_URL,
-//   TOKEN_KEY: process.env.TOKEN_KEY,
-//   DATABASE_URL: process.env.DATABASE_URL,
-// };
